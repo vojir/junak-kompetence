70f654c74a742f5fb90c56852891b0595915bf4c
Stanislav Vojíř <stanislav.vojir@gmail.com>
2016-06-05 22:05:55 +0200

    initial commit

---
+++ app/presenters/BasePresenter.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace JunakKompetence\Presenters;
+
+use Nette;
+
+
+/**
+ * Base presenter for all application presenters.
+ */
+abstract class BasePresenter extends Nette\Application\UI\Presenter
+{
+
+}
---
+++ app/presenters/HomepagePresenter.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace JunakKompetence\Presenters;
+
+use Nette;
+use App\Model;
+
+
+class HomepagePresenter extends BasePresenter
+{
+
+	public function renderDefault()
+	{
+		$this->template->anyVariable = 'any value';
+	}
+
+}
